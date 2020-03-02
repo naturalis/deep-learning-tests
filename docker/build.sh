1d8e125309499cc7ef2b0c42236581c390814789
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-02 10:44:11 +0100

    this

---
+++ docker/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo docker build -t maarten-python:latest -f Dockerfile .
---
+++ docker/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp build.sh docker-compose.yml Dockerfile requirements.txt gpu2:/data/maarten.schermer/docker
