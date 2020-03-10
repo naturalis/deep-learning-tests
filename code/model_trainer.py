44061681866f9b6219fa471b0123391cd5bd17d8
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-10 16:57:10 +0100

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         "loss": "sparse_categorical_crossentropy",
         "optimizer": tf.keras.optimizers.RMSprop(learning_rate=1e-4),
         "callbacks" : [ 
-            tf.keras.callbacks.EarlyStopping(monitor="val_acc", patience=5, mode="auto", restore_best_weights=True),
+            tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, mode="auto", restore_best_weights=True),
             tf.keras.callbacks.TensorBoard(trainer.get_tensorboard_log_path()),
             tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=4, min_lr=1e-8),
             tf.keras.callbacks.ModelCheckpoint(trainer.get_model_save_path(), monitor="val_acc", save_best_only=True, save_freq="epoch")
