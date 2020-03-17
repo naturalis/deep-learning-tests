2b3ab45458e72da8a89af5612c20225859ac0c45
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-17 10:03:40 +0100

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -153,7 +153,7 @@ class ModelTrainer():
             if self.model_settings["freeze_layers"]=="base_model":
                 for layer in self.conv_base.layers:
                     layer.trainable = False
-            elif is_integer(self.model_settings["freeze_layers"]):
+            elif self.model_settings["freeze_layers"].is_integer():
                 for layer in self.model.layers[:self.model_settings["freeze_layers"]]:
                     layer.trainable = False
 
