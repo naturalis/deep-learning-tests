3552b2417f68474fe923f499f0c63e342064d96b
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-19 15:28:36 +0100

    this

--- code/image_identify.py
+++ code/image_identify.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import sys, json
+import sys, json, os
 import tensorflow as tf
 import numpy as np
 
 class ImageIdentify:
 
+    model_path = None
+    model_name = None
     model = None
 
-    def load_model(self,filepath):
-        self.model = tf.keras.models.load_model(filepath,compile=False)
+    def set_model_path(self,path):
+        self.model_path = path
+
+    def set_model_name(self,name):
+        self.model_name = name
+
+    def load_model(self):
+        self.model = tf.keras.models.load_model(os.path.join(self.model_path, self.model_name + ".hdf5"),compile=False)
+        with open(os.path.join(self.model_path, self.model_name + "-classes.json")) as f:
+            self.classes = json.load(f)
 
     def predict(self,image):
         x = tf.keras.preprocessing.image.load_img(image, target_size=(299,299))        
         x = tf.keras.preprocessing.image.img_to_array(x)
         x = np.expand_dims(x, axis=0)
+
         prediction = self.model.predict(x)
-        y_classes = prediction.argmax(axis=-1)
-        print(y_classes)
+
+        print(prediction)
+        print(self.classes)
+
         return json.dumps(prediction.tolist())
 
 if __name__ == '__main__':
@@ -25,6 +38,8 @@ if __name__ == '__main__':
     image = sys.argv[1]
 
     predict = ImageIdentify()
-    predict.load_model("/data/corvidae/models/20200319-120518.hdf5")
+    predict.set_model_path("/data/corvidae/models")
+    predict.set_model_name("20200319-120518")
+    predict.load_model()
     x = predict.predict(image)
     print(x)
