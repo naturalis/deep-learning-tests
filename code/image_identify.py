109fba09ea934c5d7a908d4b549fe3252aa9b0cc
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-20 08:20:58 +0100

    this

--- code/image_identify.py
+++ code/image_identify.py
@@ -30,7 +30,7 @@ class ImageIdentify:
         predictions = predictions[0].tolist()
         # sort classes
         classes = {k: v for k, v in sorted(self.classes.items(), key=lambda item: item[1])}
-        predictions = dict(zip(classes.items(), predictions))
+        predictions = dict(zip(classes[0].items(), predictions))
         print(type(classes))
         print(predictions)
         return json.dumps(predictions)
