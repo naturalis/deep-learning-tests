11dafbe3a98b134fa04912776f2609f85518c114
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-03-24 17:21:57 +0100

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -379,7 +379,7 @@ if __name__ == "__main__":
         "loss": tf.keras.losses.CategoricalCrossentropy(),
         "optimizer": tf.keras.optimizers.RMSprop(learning_rate=1e-5),
         "batch_size": 64,
-        "epochs": [ 10, 200 ], # epochs single value or list controls whether training is phased
+        "epochs": [ 10, 201 ], # epochs single value or list controls whether training is phased
         "freeze_layers": [ "base_model", "none" ], # "base_model", # 249, # none
         "callbacks" : [
             [ 
