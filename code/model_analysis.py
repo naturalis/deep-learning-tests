e06c8cad6ddf0a46c89edc7fab7c5e27c15b7d7c
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-04-01 16:24:13 +0200

    this

--- code/model_analysis.py
+++ code/model_analysis.py
@@ -41,7 +41,7 @@ class ModelAnalysis(baseclass.BaseClass):
         for row in self.cm:
             self.cm_exportable.append([])
             for cell in row:
-                self.cm_exportable[i].append(str(cell.eval()))
+                self.cm_exportable[i].append(cell.numpy())
             i += 1
 
         self.cp = classification_report(self.test_generator.classes, y_pred)
