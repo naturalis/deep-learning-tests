99fe0b581936e5731b9c9423c43fe00d9cd8cb3d
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-24 13:14:14 +0200

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -252,9 +252,10 @@ if __name__ == "__main__":
         }
 
 
-    print(type(fuck))
-    print(type(os.environ["FUCK"]))
-    print(type(json.loads(os.environ["FUCK"])))
+    print(fuck)
+    fuck = json.loads(os.environ["FUCK"])
+    print(fuck)
+
 
     exit(0)
 
