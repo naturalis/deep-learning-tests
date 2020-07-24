9d5efa19cedfbf8fe05c0f041a27e1ea2e2d6af7
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-24 15:04:22 +0200

    this

--- code/lib/dataset.py
+++ code/lib/dataset.py
@@ -12,7 +12,7 @@ class DataSet():
             self.model_note = note
 
     def ask_note(self):
-        if self.model_note is None:
+        while self.model_note is None:
             self.model_note = input("enter model note: ")
 
     def make_dataset(self,model_trainer):
