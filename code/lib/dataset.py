97a0ed6264054eb04f1298e80d4beff58d9b69ee
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-27 12:16:00 +0200

    this

--- code/lib/dataset.py
+++ code/lib/dataset.py
@@ -44,7 +44,7 @@ class DataSet():
 
         # THESE SHOULD BE FULL URLS! (maybe not, what if we get a harddrive?)
         table = self.model_trainer.traindf.sort_values(by=[self.model_trainer.COL_CLASS,self.model_trainer.COL_IMAGE]).values.tolist()
-        table = list(map(lambda x: os.path.basename(x[1]), table))
+        table = list(map(lambda x: [x[0], os.path.basename(x[1]) ], table))
         print(table)
 
     
