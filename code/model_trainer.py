71e8ecf230b5bd99d1d63f92ce248039c005568a
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-28 16:13:59 +0200

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -247,7 +247,7 @@ class ModelTrainer(baseclass.BaseClass):
             phase = []
             phase.append(tf.keras.callbacks.EarlyStopping(monitor=trainer.get_preset("early_stopping_monitor"), patience=5, mode="auto", restore_best_weights=True, verbose=1))
             phase.append(tf.keras.callbacks.ModelCheckpoint(trainer.get_model_path(), monitor=trainer.get_preset("checkpoint_monitor"), save_best_only=True, save_freq="epoch", verbose=1))
-            phase.append(tf.keras.callbacks.ReduceLROnPlateau(json.loads(item)))
+            phase.append(tf.keras.callbacks.ReduceLROnPlateau(item))
             phases.append(phase)
 
         return phases
