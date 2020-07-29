b0e6b807796409832ebf215fa4c07b9a27f529f9
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-29 12:02:52 +0200

    this

--- code/lib/baseclass.py
+++ code/lib/baseclass.py
@@ -66,7 +66,7 @@ class BaseClass():
         self.debug = state
 
     def make_model_name(self):
-        return = "{0}{1:02d}{2:02d}-{3:02d}{4:02d}{5:02d}".format(
+        return "{0}{1:02d}{2:02d}-{3:02d}{4:02d}{5:02d}".format(
             self.timestamp.year,
             self.timestamp.month,
             self.timestamp.day,
