b239f3ec7b6870f79ff8f6416b2bebfc35e61d83
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-07-30 12:35:08 +0200

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
 
     if args.load_model: 
 
-        self.model_settings["freeze_layers"] = None
+        trainer.model_settings["freeze_layers"] = None
 
         trainer.load_model()
         trainer.read_class_list()
