6b53d3cff7b22165330c0a318f3238117892a51d
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-08-03 11:09:22 +0200

    this

--- code/model_compare.py
+++ code/model_compare.py
@@ -6,31 +6,76 @@ from lib import baseclass
 
 class ModelCompare(baseclass.BaseClass):
 
+    names = []
+    dates = []
+    notes = []
+    classes = []
+    epochs = []
+
     def __init__(self):
         super().__init__()
 
-    def fuck(self):
+    def print_data(self):
+        names = ""
+        dates = ""
+        notes = ""
+        classes = ""
+        epochs = ""
+        for x in range(len(self.names)):
+            names += "{<30}"
+            dates += "{<30}"
+            notes += "{<30}"
+            classes += "{>30}"
+            epochs += "{>30}"
+        print(names.format(*self.names))
+        print(dates.format(*self.dates))
+        print(notes.format(*self.notes))
+        print(classes.format(*self.classes))
+        # self.dates = []
+        # self.notes = []
+        # self.classes = []
+        # self.epochs = []
+
+    def collect_data(self):
+        self.names = []
+        self.dates = []
+        self.notes = []
+        self.classes = []
+        self.epochs = []
+
         for entry in os.scandir(self.models_folder):
             analysis = os.path.join(self.models_folder, entry.name, "analysis.json")
             classes = os.path.join(self.models_folder, entry.name, "classes.json")
             dataset = os.path.join(self.models_folder, entry.name, "dataset.json")
             if os.path.exists(dataset):
                 with open(dataset) as json_file:
-                    dataset = json.load(json_file)
-                    print("{} ({}): {}".format(dataset["model_name"],dataset["created"],dataset["model_note"].strip()))
-                    print("{} classes (min {}, max {} images)".
-                        format(
+                    self.dataset = json.load(json_file)
+                    self.names.append(dataset["model_name"])
+                    self.dates.append(dataset["created"])
+                    self.notes.append(dataset["model_note"])
+                    self.classes.append("{} ({}/{})".format(
                             dataset["class_count"],
                             dataset["class_image_minimum"],
                             dataset["class_image_maximum"]
-                           )
+                       )
                     )
-                    print("trained {} epochs ({})".
-                        format(
-                            "; ".join(map(str,dataset["training_phases"]["epochs"])),
-                            "; ".join(dataset["training_phases"]["freeze_layers"])
-                        )
-                    )
+                    # epochs.append()
+
+
+                    # print("{} ({}): {}".format(dataset["model_name"],dataset[""],dataset["model_note"].strip()))
+                    # print("{} classes (min {}, max {} images)".
+                        # format(
+                        #     dataset["class_count"],
+                        #     dataset["class_image_minimum"],
+                        #     dataset["class_image_maximum"]
+                        #    )
+                    # )
+                    # print("trained {} epochs ({})".
+                    #     format(
+                    #         "; ".join(map(str,dataset["training_phases"]["epochs"])),
+                    #         "; ".join(dataset["training_phases"]["freeze_layers"])
+                    #     )
+                    # )
                     
         
 #         scan dirs
@@ -55,5 +100,5 @@ if __name__ == "__main__":
 
     compare.set_debug(os.environ["DEBUG"]=="1" if "DEBUG" in os.environ else False)
     compare.set_project(os.environ)
-    compare.fuck()
-
+    compare.collect_data()
+    compare.print_data()
