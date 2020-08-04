58551dc0a1090c044640f72e98ec1987f5f469a7
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-08-04 16:12:13 +0200

    this

--- code/project_prepare.py
+++ code/project_prepare.py
@@ -7,8 +7,10 @@ class ProjectPrepare(baseclass.BaseClass):
 
     def __init__(self):
         super().__init__()
+
+    def print_project_name(self):
         self.logger.info(self.project_name)
-        self.logger.info(self.project_root)
+        # self.logger.info(self.project_root)
 
     def make_folders(self):
         for folder in self.folders:
@@ -24,4 +26,5 @@ if __name__ == "__main__":
 
     prepare = ProjectPrepare()
     prepare.set_project(os.environ)
+    prepare.print_project_name()
     prepare.make_folders()
