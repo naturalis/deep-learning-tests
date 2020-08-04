a2d1d6128d3052b010d74f1744fad253ccf59e05
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-08-04 16:09:35 +0200

    this

--- code/project_prepare.py
+++ code/project_prepare.py
@@ -15,9 +15,9 @@ class ProjectPrepare(baseclass.BaseClass):
             if not os.path.exists(f):
                 os.mkdir(f)
                 os.chmod(f,0o777)
-                self.logger.info("created folder {}".format(f))
+                self.logger.info("created folder \"{}\".format(f))
             else:
-                self.logger.info("folder {} already exists".format(f))
+                self.logger.info("folder \"{}\" already exists".format(f))
 
 if __name__ == "__main__":
 
