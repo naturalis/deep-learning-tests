95d3ff613a687eca0ff1b24e88a3fea8babd32d3
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-08-12 13:24:00 +0200

    this

--- code/image_identify.py
+++ code/image_identify.py
@@ -5,14 +5,43 @@ from lib import baseclass
 
 class ImageIdentify(baseclass.BaseClass):
 
-    model_path = None
-    model_name = None
-    model = None
+    images = []
+    results = []
 
-    def predict(self,image):
-        x = tf.keras.preprocessing.image.load_img(image, target_size=(299,299))        
+    def set_image(self,image_path):
+        self.images.append(image_path)
+        print(self.images)
+
+    def set_image(self,image_paths):
+        for item in image_paths.split(","):
+            self.images.append(item)
+        print(self.images)
+
+    def set_image_list(self,list_path):
+        file1 = open(list_path,"r+")  
+        self.images.append(file1.readlines)
+        print(self.images)
+
+    def predict_images(self):
+        self.results = []
+        for image in self.images:
+            predictions = self.predict_image(image)
+            self.results.append({ "image" : image, "predictions" : predictions })
+        return json.dumps({ "project" : self.project_name, "model" : self.model_name, "predictions" : self.results })
+
+    def predict_image(self,image,json_encode=False):
+        x = tf.keras.preprocessing.image.load_img(
+            image, 
+            target_size=(299,299),
+            interpolation="nearest")
         x = tf.keras.preprocessing.image.img_to_array(x)
         x = np.expand_dims(x, axis=0)
+
+        x = x[..., :3]  # remove alpha channel if present
+        if x.shape[3] == 1:
+            x = np.repeat(x, axis=3, repeats=3)
+        x /= 255.0
+        # x = (x - 0.5) * 2.0 # why this, laurens?
 
         predictions = self.model.predict(x)
         predictions = predictions[0].tolist()
@@ -20,7 +49,7 @@ class ImageIdentify(baseclass.BaseClass):
         predictions = dict(zip(classes.keys(), predictions))
         predictions = {k: v for k, v in sorted(predictions.items(), key=lambda item: item[1], reverse=True)}
 
-        return json.dumps(predictions)
+        return json.dumps(predictions) if json_encode else predictions
 
 if __name__ == '__main__':
 
@@ -28,18 +57,32 @@ if __name__ == '__main__':
     predict.set_debug(os.environ["DEBUG"]=="1" if "DEBUG" in os.environ else False)
     predict.set_project(os.environ)
 
-    image = sys.argv[1]
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--image", type=str)
+    parser.add_argument("--images", type=str)
+    parser.add_argument("--list", type=str)
+    parser.add_argument("--model", type=str)
+    args = parser.parse_args() 
 
-    if len(sys.argv)>2:
-        model_name = sys.argv[2]
-    else:
-        model_name = os.environ['MODEL_NAME']
-
-    predict.set_model_name(model_name)
+    if args.model:
+        predict.set_model_name(args.model)
+    else
+        predict.set_model_name(os.environ['MODEL_NAME'])
 
     predict.load_model()
 
-    x = predict.predict(image)
+    if args.image:
+        # predict.set_image(args.image)
+        x = predict.predict_image(image)
+
+    if args.images:
+        predict.set_images(args.images)
+        x = predict.predict_images(image)
+
+    if args.list:
+        predict.set_image_list(args.list)
+        x = predict.predict_images(image)
+
     print(x)
 
     # export PROJECT_ROOT=/data/ai/corvidae/
