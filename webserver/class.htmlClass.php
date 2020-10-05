0c29d071d7590b9e3689b485f8e692306b3866c6
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-10-05 13:04:38 +0200

    this

---
+++ webserver/class.htmlClass.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    class HtmlClass
+    {
+        function header()
+        {
+            return <<< EOT
+<html>
+<link rel="stylesheet" type="text/css" href="style.css" >
+<head>
+<body>
+EOT;
+        }
+
+
+    }
--- webserver/index.php
+++ webserver/index.php
@@ -1,59 +1,18 @@
 <?php
 
-    class BaseClass {
-
-        private $projectRoot;
-        private $projectName;
-        private $ImagesRoot;
-
-        public function __init()
-        {
-        }
-
-        public function setProjectRoot($var)
-        {
-            $this->projectRoot = rtrim($var,"/");
-        }
-
-        public function setProjectName($var)
-        {
-            $this->projectName = $var;
-        }
-
-        public function setImagesRoot($var)
-        {
-            $this->ImagesRoot = $var;
-        }
-
-        public function getProjectRoot()
-        {
-            return $this->projectRoot;
-        }
-
-        public function getProjectName()
-        {
-            return $this->projectName;
-        }
-
-        public function getImagesRoot()
-        {
-            return $this->ImagesRoot;
-        }
-
-        public function getModels()
-        {
-            return array_filter(scandir(implode("/",[$this->getProjectRoot(),"models"])),function($a){ return !in_array($a,[".",".."]);});
-        }
-
-
-    }
-
-
+    include_once("class.baseClass.php");
+    include_once("class.htmlClass.php");
 
     $base = new BaseClass;
     $base->setProjectRoot(getenv('PROJECT_ROOT'));
     $base->setProjectName(getenv('PROJECT_NAME'));
     $base->setImagesRoot(getenv('IMAGES_ROOT'));
+
+    $html = new HtmlClass;
+
+    echo $html->header()
+
+
 
     echo $base->getProjectName() . "<br />";
     echo $base->getProjectRoot() . "<br />";
