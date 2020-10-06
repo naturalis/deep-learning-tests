fabebb629b4d9ad4c1a11290544a6bbe40ec4b31
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-10-06 13:15:57 +0200

    this

--- webserver/model.php
+++ webserver/model.php
@@ -25,23 +25,25 @@
 
     $r=[];
 
-    $r[1][] = [ "html" => "accuracy:" ];
-    $r[1][] = [ "html" => $analysis["classification_report"]["accuracy"] ];
-    $r[2][] = [ "html" => "precision" ];
+    $r[0][] = [ "html" => "accuracy:" ];
+    $r[0][] = [ "html" => $analysis["classification_report"]["accuracy"] ];
+    $r[1][] = [ "html" => "macro" ];
+    $r[2][] = [ "html" => "precision:" ];
     $r[2][] = [ "html" => $analysis["classification_report"]["macro avg"]["precision"] ];
-    $r[3][] = [ "html" => "recall" ];
+    $r[3][] = [ "html" => "recall:" ];
     $r[3][] = [ "html" => $analysis["classification_report"]["macro avg"]["recall"] ];
-    $r[4][] = [ "html" => "f1-score" ];
+    $r[4][] = [ "html" => "f1-score:" ];
     $r[4][] = [ "html" => $analysis["classification_report"]["macro avg"]["f1-score"] ];
-    $r[5][] = [ "html" => "support" ];
+    $r[5][] = [ "html" => "support:" ];
     $r[5][] = [ "html" => $analysis["classification_report"]["macro avg"]["support"] ];
-    $r[6][] = [ "html" =>  "precision" ];
+    $r[10][] = [ "html" =>  "weighted:" ];
+    $r[6][] = [ "html" =>  "precision:" ];
     $r[6][] = [ "html" => $analysis["classification_report"]["weighted avg"]["precision"] ];
-    $r[7][] = [ "html" => "recall" ];
+    $r[7][] = [ "html" => "recall:" ];
     $r[7][] = [ "html" => $analysis["classification_report"]["weighted avg"]["recall"] ];
-    $r[8][] = [ "html" => "f1-score" ];
+    $r[8][] = [ "html" => "f1-score:" ];
     $r[8][] = [ "html" => $analysis["classification_report"]["weighted avg"]["f1-score"] ];
-    $r[9][] = [ "html" => "support" ];
+    $r[9][] = [ "html" => "support:" ];
     $r[9][] = [ "html" => $analysis["classification_report"]["weighted avg"]["support"] ];
 
     echo $html->p($html->table($r,"analysis"));
