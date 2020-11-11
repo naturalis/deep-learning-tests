a894bc79028975c2a594622ffe891168d00f42cc
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-11-11 16:06:12 +0100

    this

--- code/model_analysis.py
+++ code/model_analysis.py
@@ -30,7 +30,6 @@ class ModelAnalysis(baseclass.BaseClass):
             target_size=(299, 299),
             batch_size=self.model_settings["batch_size"],
             interpolation="nearest",
-            subset="validation",
             shuffle=False
         )
 
