3f864c7501c406f660d18b86fdf8beadd2b5078b
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-11-16 15:44:28 +0100

    this

--- code/model_compare.py
+++ code/model_compare.py
@@ -261,7 +261,7 @@ class ModelCompare(baseclass.BaseClass):
         #     self.models = sorted(self.models, key=lambda k: (str(k[self.sort]),str(k["accuracy"]),str(k["date"])))
 
         def get_age(employee):
-            return employee.get('name')
+            return (int(employee.get('name')),int(employee.get('accuracy') * 100))
 
         self.models = sorted(self.models, key=get_age)
 
