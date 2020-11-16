4d6acd5370ab8c017f282ab754647aa66e56831f
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-11-16 15:46:21 +0100

    this

--- code/model_compare.py
+++ code/model_compare.py
@@ -262,7 +262,7 @@ class ModelCompare(baseclass.BaseClass):
 
         def get_age(employee):
             print("------->",int(employee.get('name').replace("-","")),int(employee.get('accuracy') * 100))
-            return (int(employee.get('name').replace("-","")),int(employee.get('accuracy') * 100))
+            return (int(employee.get('name').replace("-")),int(employee.get('accuracy') * 100))
 
         self.models = sorted(self.models, key=get_age)
 
