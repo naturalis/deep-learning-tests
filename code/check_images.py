72c98e794b08085353d5cfdaac21ec4d7c0664fe
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-12-01 16:58:31 +0100

    this

--- code/check_images.py
+++ code/check_images.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     dataset.logger.info("checking for corrupt files")
 
     if args.path:
-        dataset.logger.info("checking for corrupt files in {}".format(dataset.image_path))
-        bad_files = utils.verify_images(dataset.image_path)
-    else:
         dataset.logger.info("checking for corrupt files in {}".format(args.path))
         bad_files = utils.verify_images(args.path)
+    else:
+        dataset.logger.info("checking for corrupt files in {}".format(dataset.image_path))
+        bad_files = utils.verify_images(dataset.image_path)
 
     for file in bad_files:
         dataset.logger.info("found corrupt file: {} ({})".format(file["filename"],file["error"]))
