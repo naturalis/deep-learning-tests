b271be78b1eb1e929f1bb8a808572c095bdabbc5
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-12-01 16:56:58 +0100

    this

--- code/check_images.py
+++ code/check_images.py
@@ -1,7 +1,11 @@
-import os
+import os, argparse
 from lib import baseclass, utils, dataset
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--path",type=str)
+    args = parser.parse_args()
 
     dataset = dataset.DataSet()
     dataset.set_environ(os.environ)
@@ -10,7 +14,12 @@ if __name__ == "__main__":
 
     dataset.logger.info("checking for corrupt files")
 
-    bad_files = utils.verify_images(dataset.image_path)
+    if args.path:
+        dataset.logger.info("checking for corrupt files in {}".format(dataset.image_path))
+        bad_files = utils.verify_images(dataset.image_path)
+    else:
+        dataset.logger.info("checking for corrupt files in {}".format(args.path))
+        bad_files = utils.verify_images(args.path)
 
     for file in bad_files:
         dataset.logger.info("found corrupt file: {} ({})".format(file["filename"],file["error"]))
