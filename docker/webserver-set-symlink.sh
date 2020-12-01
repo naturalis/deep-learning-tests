7442de9276607511335e23ea459ea4be3c7f8468
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-12-01 11:10:19 +0100

    this

--- docker/webserver-set-symlink.sh
+++ docker/webserver-set-symlink.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-
 PROJECT_ROOT=$(grep PROJECT_ROOT .env | cut -d '=' -f2)
 
+sudo docker-compose run  webserver /bin/sh -c "rm /var/www/html/project"
+sudo docker-compose run  webserver /bin/sh -c "ln -s ${PROJECT_ROOT} /var/www/html/project"
 
-
-
-# sudo docker-compose run  webserver /bin/sh -c 'rm /var/www/html/project'
-# sudo docker-compose run  webserver /bin/sh -c 'ln -s $PROJECT_ROOT /var/www/html/project'
-
-echo sudo docker-compose run  webserver /bin/sh -c 'ln -s $PROJECT_ROOT /var/www/html/project'
