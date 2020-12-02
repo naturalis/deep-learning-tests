c518ad9abf88bf1396ef76a1864fe26ccc8e70cf
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-12-02 18:03:31 +0100

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -140,8 +140,7 @@ class ModelTrainer(baseclass.BaseClass):
             dataset_other = self.traindf[self.traindf[self.COL_CLASS]!=minority_label]
             dataset_minority_upsampled = resample(dataset_minority,
                                                     replace=True,
-                                                    n_samples=int(np.ceil(len(dataset_minority) * factor)),
-                                                    random_state=111)
+                                                    n_samples=int(np.ceil(len(dataset_minority) * factor)))
 
             self.traindf = pd.concat([dataset_minority_upsampled, dataset_other])
             self.logger.debug("upsampling, dataset num rows: {}".format(len(self.traindf)))
@@ -156,8 +155,7 @@ class ModelTrainer(baseclass.BaseClass):
             dataset_other = self.traindf[self.traindf[self.COL_CLASS]!=majority_label]
             dataset_majority_downsampled = resample(dataset_majority,
                                                     replace=False,
-                                                    n_samples=int(np.ceil(len(dataset_majority) * factor)),
-                                                    random_state=111)
+                                                    n_samples=int(np.ceil(len(dataset_majority) * factor)))
 
             self.traindf = pd.concat([dataset_majority_downsampled, dataset_other])
             self.logger.debug("downsampling, dataset num rows : {}".format(len(self.traindf)))
