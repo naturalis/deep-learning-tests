c2f4513b5a389d00e090d03a98f277182fc9c083
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2020-12-08 15:27:32 +0100

    this

--- scripts/resize_images.sh
+++ scripts/resize_images.sh
@@ -11,7 +11,7 @@ IFS=$'\n'
 for f in $(find $INDIR -type f -name '*.jpg');
 do
     new_f=$(echo "$f" | sed 's/ /\\ /g')
-    convert "$new_f" -resize 500x500^ "$new_f"
-    echo "$new_f"
+    convert $new_f -resize 500x500^ $new_f
+    echo $new_f
 done
 
