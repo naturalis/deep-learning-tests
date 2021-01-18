4ea5627cec6c73f64ec73dd33245755dcd96cf73
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2021-01-18 17:44:30 +0100

    this

--- code/model_trainer.py
+++ code/model_trainer.py
@@ -421,23 +421,14 @@ class ModelTrainer(baseclass.BaseClass):
 
             if not self.skip_training:
 
-                # history = self.model.fit(
-                #     x=self.train_generator,
-                #     steps_per_epoch=step_size_train,
-                #     epochs=epoch,
-                #     validation_data=self.validation_generator,
-                #     validation_steps=step_size_validate,
-                #     callbacks=self.current_callbacks,
-                #     class_weight=self.class_weight
-                # )
-
                 history = self.model.fit(
                     x=self.train_generator,
                     steps_per_epoch=step_size_train,
                     epochs=epoch,
                     validation_data=self.validation_generator,
                     validation_steps=step_size_validate,
-                    callbacks=self.current_callbacks
+                    callbacks=self.current_callbacks,
+                    class_weight=self.class_weight
                 )
 
                 # If x is a dataset, generator, or keras.utils.Sequence instance, y should not be specified (since targets
