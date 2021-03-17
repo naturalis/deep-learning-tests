070e33bc6d77c0bb4c22f7512840b63b72b063bb
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2021-03-17 16:00:45 +0100

    this

--- code/image_identify.py
+++ code/image_identify.py
@@ -126,13 +126,6 @@ class ImageIdentify(baseclass.BaseClass):
                 results_batch.append({ 'class' : key, 'prediction': predictions_batch[key] })
 
         if not results_batch is None:
-            logger.info("prediction (batch): {}".format(results_batch[0]))
-        if not results_original is None:
-            logger.info("prediction (original): {}".format(results_original[0]))
-
-        logger.info("time taken: {}".format(perf_counter()-prediction_start))
-
-        if not results_batch is None:
             output = { 'predictions' : results_batch }
             if not results_original is None:
                 output['predictions_original'] = results_original
