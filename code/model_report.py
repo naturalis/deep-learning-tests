e37f3fde0a6c742f4e9b252214d1d487ba90ab15
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2021-03-22 15:31:09 +0100

    this

--- code/model_report.py
+++ code/model_report.py
@@ -27,7 +27,7 @@ class ModelReport(baseclass.BaseClass):
             all_classes = list(reader)
 
         for key,val in used_classes.items():
-            match = [ x for x in all_classes if x[0] == key ]
+            match = [ x for x in all_classes if 0 in x and x[0] == key ]
             self.classes.append({"key" : val, "class" : key, "support" : int(match[0][1]) })
 
         for item in all_classes:
