682417ccc6b5e210846cb064435946d3423c9a8b
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2021-03-30 12:53:53 +0200

    this

--- code/lib/utils.py
+++ code/lib/utils.py
@@ -123,8 +123,8 @@ class ImageVerifier:
             # reader = csv.reader(csv_file, delimiter=utils._determine_csv_separator(self.downloaded_images_file,"utf-8-sig"))
             reader = csv.reader(csv_file, delimiter=_determine_csv_separator(self.image_list_file,"utf-8-sig"))
             for row in reader:
-                if not row or self.filepath_col not in row:
-                    continue
+                # if not row or self.filepath_col not in row:
+                #     continue
                 file = row[self.filepath_col]
                 print(row)
                 print(file)
